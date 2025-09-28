56b26116caf5bd193fa9efe867da0644f88008e1
karentasmaniamalutalo-ship-it <karentasmaniamalutalo@gmail.com>
2025-09-29 11:30:47 +1300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,12 @@
+// validation
+const { body, query, param, validationResult } = require('express-validator');
+
+function validate(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+  next();
+}
+
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const { User, LeaveBalance, LeaveType, DefaultBalance } = require('../models');
